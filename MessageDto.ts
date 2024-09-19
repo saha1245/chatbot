d0aec442c1cf40bc17d02b478157e19dcf4cf789
saha1245 <104314031+saha1245@users.noreply.github.com>
2024-09-19 13:10:18 +0530

    Add files via upload

---
+++ MessageDto.ts
@@ -0,0 +1,10 @@
+export class MessageDto {
+    isUser: boolean;
+    content: string;
+  
+    constructor(isUser: boolean, content: string) {
+      this.isUser = isUser;
+      this.content = content;
+    }
+  }
+export default MessageDto;
